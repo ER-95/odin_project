85bc199b75d9b87174e159b9167579c08dbcad91
ER <53078703+ER-95@users.noreply.github.com>
2023-09-07 21:31:58 +0100

    Updated RPS game

--- 13-rock-paper-scissors/javascript.js
+++ 13-rock-paper-scissors/javascript.js
@@ -17,36 +17,63 @@ function getPlayerChoice() {
 getPlayerChoice();
 
 function playRound(playerChoice, computerChoice) {
-  if (computerChoice == "rock") {
-    if (playerChoice == "paper") {
-      winner = "player";
-    } else if (playerChoice == "scissors") {
-      winner = "computer";
-    } else if (playerChoice == "rock") {
-      winner = "no-one";
+  if (computerChoice == playerChoice) {
+    winner = "draw";
+  } else if (computerChoice == "rock") {
+      if (playerChoice == "paper") {
+        winner = "player";
+      } else if (playerChoice = "scissors") {
+        winner = "computer";
+      }
+  } else if (computerChoice == "paper") {
+      if (playerChoice == "rock") {
+        winner = "computer";
+      } else if (playerChoice = "scissors") {
+        winner = "player";
+      }
+  } else if (computerChoice == "scissors") {
+      if (playerChoice == "rock") {
+        winner = "player";
+      } else if (playerChoice = "paper") {
+        winner = "computer";
+      }
     }
-  }
-  if (computerChoice == "scissors") {
-    if (playerChoice == "paper") {
-      winner = "computer";
-    } else if (playerChoice == "scissors") {
-      winner = "no-one";
-    } else if (playerChoice == "rock") {
-      winner = "player";
-    }
-  }
-  if (computerChoice == "paper") {
-    if (playerChoice == "paper") {
-      winner = "no-one";
-    } else if (playerChoice == "scissors") {
-      winner = "player";
-    } else if (playerChoice == "rock") {
-      winner = "computer";
-    }
-  }
   console.log("Computer picked " + computerChoice);
   console.log("Player picked " + playerChoice);
-  console.log("Winner is " + winner)
+  if (winner == "draw") {
+    console.log("No one wins. Both players picked the same option");
+  } else {
+    console.log("Winner is " + winner);
+  }
 }
 
 playRound(playerChoice, computerChoice);
+
+// The below is my original code for this, but it has a lot of unnecessary duplications (e.g. we can declare a draw by writing out an if to compare playerChoice to computerChoice once rather than three times)
+  // if (computerChoice == "rock") {
+  //   if (playerChoice == "paper") {
+  //     winner = "player";
+  //   } else if (playerChoice == "scissors") {
+  //     winner = "computer";
+  //   } else if (playerChoice == "rock") {
+  //     winner = "draw";
+  //   }
+  // }
+  // if (computerChoice == "scissors") {
+  //   if (playerChoice == "paper") {
+  //     winner = "computer";
+  //   } else if (playerChoice == "scissors") {
+  //     winner = "draw";
+  //   } else if (playerChoice == "rock") {
+  //     winner = "player";
+  //   }
+  // }
+  // if (computerChoice == "paper") {
+  //   if (playerChoice == "paper") {
+  //     winner = "draw";
+  //   } else if (playerChoice == "scissors") {
+  //     winner = "player";
+  //   } else if (playerChoice == "rock") {
+  //     winner = "computer";
+  //   }
+  // }
